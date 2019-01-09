fe786467b706c552ce1e3cc4fadb23910606e370
Kartik Ravichander <kartik.ravichander@Kartiks-MBP.attlocal.net>
2019-01-09 13:21:55 -0500

    eleventh commit

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ let city = 'portland';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 let port = process.env.PORT || 5000;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -32,6 +37,7 @@ app.post("/webhook", function(req, res) {
         console.log('body:', body);
         var weather =JSON.parse(body);
         temp_min = weather.main.temp_min;
+
         return res.json({
             "fulfillmentText": "",
             "fulfillmentMessages": [
@@ -48,8 +54,41 @@ app.post("/webhook", function(req, res) {
 
       }
     });
-
-
 });
 
-app.listen(port, () => console.log(`My dialogflow Example app listening on port ${port}`))
+
+app.post("/webhookSocket", function(req, res) {
+    console.log('request body  channel'+req.body.queryResult.parameters['channel']);
+    let channel =req.body.queryResult.parameters['channel'];
+    request(url, function (err, response, body) {
+      if(err){
+        console.log('error:', error);
+      } else {
+        console.log('body:', body);
+        var weather =JSON.parse(body);
+        temp_min = weather.main.temp_min;
+        io.on('connection', function (socket) {
+          console.log('Socket on connection with client!!!!!!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+          socket.emit('fromServer', { 'channel': channel });
+        });
+
+
+        return res.json({
+            "fulfillmentText": "",
+            "fulfillmentMessages": [
+                {
+                  "text": {
+                    "text": [
+                        "Text is coming from webhook Heroku.Adding channel "+channel+"and the price is "+temp_min
+                      ]
+                    }
+                }
+              ],
+            source: "googledilagoueapp"
+          });
+
+      }
+    });
+});
+
+app.listen(port, () => console.log(`Googledialogflow Example app!! listening on port ${port}`));
